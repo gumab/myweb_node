983467a259b86c6768d1b336f741fd887ae07d9b
guma <vuster8@gmail.com>
2015-11-21 19:39:15 +0900

    env

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,14 @@
+'use strict';
+
 // config/database.js
+var host = 'gumabae.iptime.org';
+if (process.env.NODE_ENV !== 'production') {
+  host = '127.0.0.1';
+}
+
 module.exports = {
   mysql: {
-    host: 'gumabae.iptime.org',
+    host: host,
     port: 3306,
     user: 'myweb',
     password: 'myweb1!',
