a79a87fde1a25ce446f670d8613aa44cd96d2e88
guma <vuster8@gmail.com>
2015-12-02 00:00:04 +0900

    disk info check

--- server/routes/api/authed.js
+++ server/routes/api/authed.js
@@ -2,12 +2,25 @@
 
 var config = require('../../config/config');
 var router = require('express').Router();
+var requestHelper = require('../request-helper')();
+var storage = require('storage-device-info');
 
 
 router.get('/profile', function (req, res, next) {
   res.send(JSON.stringify(req.user));
 });
 
+router.get('/diskInfo', function (req, res, next) {
+  var R = requestHelper(req);
+  var path = req.query.path || '/';
+  storage.getPartitionSpace(path, function (error, space) {
+    if (error) {
+      next(error);
+    } else {
+      res.json(R.getJSONResponse('000', '', space));
+    }
+  });
+});
 
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   //app.use('/ko/page1', require('./page/page1'));
   //app.use('/ko/page2', require('./page/page2'));
   //app.use('/ko/login', require('./page/login'));
-  app.get('/', function(req,res,next){
+  app.get('/', function (req, res, next) {
     res.redirect('/admin');
   });
 
