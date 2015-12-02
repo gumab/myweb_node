4dc505db0c5de63a698046b92278520959767206
chabae <chabae@ebay.com>
2015-12-02 09:32:58 +0900

    favicon

--- server/routes/api/authed.js
+++ server/routes/api/authed.js
@@ -3,7 +3,7 @@
 var config = require('../../config/config');
 var router = require('express').Router();
 var requestHelper = require('../request-helper')();
-var ndjs = require('nodejs-disks');
+var deviceService = require('../../services/deviceService');
 
 
 router.get('/profile', function (req, res, next) {
@@ -12,23 +12,12 @@ router.get('/profile', function (req, res, next) {
 
 router.get('/diskInfo', function (req, res, next) {
   var R = requestHelper(req);
-
-  ndjs.drives(function (err, driveNames) {
-    var drives = driveNames.filter(function (value, index, arr) {
-      if (value.indexOf('/dev/sd') < 0 && value.indexOf('/dev/root') < 0) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-
-    ndjs.drivesDetail(drives, function (err, driveDetails) {
-      if (err) {
-        next(err);
-      } else {
-        res.json(R.getJSONResponse('000', '', driveDetails));
-      }
-    });
+  deviceService.getDiskInfo(function(err,data){
+    if (err) {
+      next(err);
+    } else {
+      res.json(R.getJSONResponse('000', '', data));
+    }
   });
 });
 
--- server/services/deviceService.js
+++ server/services/deviceService.js
@@ -1,32 +1,41 @@
 'use strict';
 
 var MyWebError = require('../lib/middlewares/error-handler').MyWebError;
-var ndjs = require('nodejs-disks');
+var ndjs;
+
+try {
+  ndjs = require('nodejs-disks');
+} catch(e) {
+  ndjs = '';
+}
 
 module.exports = {
 
   getDiskInfo: function (callback) {
+    if (ndjs) {
+      ndjs.drives(function (err, driveNames) {
+        if (err) {
+          callback(err);
+        } else {
+          var drives = driveNames.filter(function (value, index, arr) {
+            if (value.indexOf('/dev/sd') < 0 && value.indexOf('/dev/root') < 0) {
+              return false;
+            } else {
+              return true;
+            }
+          });
 
-    ndjs.drives(function (err, driveNames) {
-      if (err) {
-        callback(err);
-      } else {
-        var drives = driveNames.filter(function (value, index, arr) {
-          if (value.indexOf('/dev/sd') < 0 && value.indexOf('/dev/root') < 0) {
-            return false;
-          } else {
-            return true;
-          }
-        });
-
-        ndjs.drivesDetail(drives, function (err, driveDetails) {
-          if (err) {
-            callback(err);
-          } else {
-            callback(null, driveDetails);
-          }
-        });
-      }
-    });
+          ndjs.drivesDetail(drives, function (err, driveDetails) {
+            if (err) {
+              callback(err);
+            } else {
+              callback(null, driveDetails);
+            }
+          });
+        }
+      });
+    } else {
+      callback(null, []);
+    }
   }
 };
